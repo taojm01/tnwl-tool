9db85c9722357076529ae3920d1518c1ce6d50e9
taojm <88290122@qq.com>
2023-06-25 15:29:19 +0800

    docker

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,18 @@
 $imageName="tnwl-tool"
 
-./gradlew jib --image=$imageName
+./gradlew jibDockerBuild
 
 if (-not $?) {
     Write-Warning("build image failed")
     return
 }
+# docker-compose stop $imageName
+# docker-compose rm -f $imageName
+
+docker-compose up -d $imageName
+if (-not $?) {
+    Write-Warning("{0} start failed" -f $imageName)
+    return
+}
+docker image rm $(docker image ls -a -q)
+docker-compose logs -f $imageName
